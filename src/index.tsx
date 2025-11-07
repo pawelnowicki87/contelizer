b0cb853bb7e3ef8b24acd801ff0625674263fbf3
Pawel Nowicki <pawel.nowicki87@tlen.pl>
2025-11-07 23:55:05 +0100

    Fix Redux Provider and deploy setup

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,20 @@
+import React from "react";
 import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux";
 import { HashRouter } from "react-router-dom";
 import App from "./App";
+import { store } from "./app/store";
 
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
 root.render(
-  <HashRouter>
-    <App />
-  </HashRouter>
+  <React.StrictMode>
+    <Provider store={store}>
+      <HashRouter>
+        <App />
+      </HashRouter>
+    </Provider>
+  </React.StrictMode>
 );
